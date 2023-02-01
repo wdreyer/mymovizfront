f4fdcb21e3a6a953a3746886964a77dd0e308f50
Your name <dreyer.wil@gmail.com>
2023-02-01 11:41:27 +0100

    update fetch

--- components/Home.js
+++ components/Home.js
@@ -20,7 +20,7 @@ function Home() {
 
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-moviez-back.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         const formatedData = data.movies.map(movie => {
